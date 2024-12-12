d94bd858c45a5530555d9a03b49cb06390564764
minlearnminlearn <minlearn@minlearn.org>
2024-12-12 16:53:57 +0800

    initial

---
+++ traffmonetizer_install.sh
@@ -0,0 +1,45 @@
+###############
+
+echo "Installing Dependencies"
+apt-get install -y curl
+apt-get install -y sudo
+apt-get install -y mc
+echo "Installed Dependencies"
+
+mkdir -p /usr/lib/x86_64-linux-musl
+wget --no-check-certificate https://github.com/minlearn/traffmonetizer/raw/master/tm.tar.gz -O /tmp/tm.tar.gz
+tar -xzvf /tmp/tm.tar.gz -C /usr/lib/x86_64-linux-musl tm/app/usrlibx86_64-linux-musl/{libz.so.1,libstdc++.so.6,libssl.so.1.1,libgcc_s.so.1,libcrypto.so.1.1} --strip-components=3
+tar -xzvf /tmp/tm.tar.gz -C /lib tm/app/lib/ld-musl-x86_64.so.1 --strip-components=3
+tar -xzvf /tmp/tm.tar.gz -C /etc tm/app/etc/ld-musl-x86_64.path --strip-components=3
+tar -xzvf /tmp/tm.tar.gz -C /usr/local/bin tm/app/Cli --strip-components=2
+rm -rf /tmp/tm.tar.gz
+
+cat > /lib/systemd/system/tm.service << 'EOL'
+[Unit]
+Description=this is Traffmonetizer service,please change the token then daemon-reload it
+After=network.target nss-lookup.target
+Wants=network.target nss-lookup.target
+Requires=network.target nss-lookup.target
+
+[Service]
+Environment="DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=true"
+Environment="DOTNET_CLI_TELEMETRY_OPTOUT=1"
+Type=simple
+ExecStartPre=/usr/bin/sleep 2
+ExecStart=/usr/bin/bash -c "PATH=/usr/local/bin:\$PATH exec /usr/local/bin/Cli start accept --token ed0ja2kTXg9QE51hL+I6KWqKlv+wuZ/G2aS8yF/mi/M="
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+systemctl enable tm
+systemctl start tm
+
+
+echo "Cleaning up"
+apt-get -y autoremove
+apt-get -y autoclean
+echo "Cleaned"
+
+##############
