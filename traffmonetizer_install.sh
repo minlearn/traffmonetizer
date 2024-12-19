08a820041f177476b2568cbe040291b1982ad3d8
minlearnminlearn <minlearn@minlearn.org>
2024-12-20 00:38:42 +0800

    Update traffmonetizer_install.sh

--- traffmonetizer_install.sh
+++ traffmonetizer_install.sh
@@ -6,6 +6,7 @@ echo "Installing Dependencies"
 silent apt-get install -y curl sudo mc
 echo "Installed Dependencies"
 
+echo "Installing tm"
 mkdir -p /usr/lib/x86_64-linux-musl
 wget --no-check-certificate https://github.com/minlearn/traffmonetizer/raw/master/tm.tar.gz -O /tmp/tm.tar.gz
 tar -xzvf /tmp/tm.tar.gz -C /usr/lib/x86_64-linux-musl tm/app/usrlibx86_64-linux-musl/{libz.so.1,libstdc++.so.6,libssl.so.1.1,libgcc_s.so.1,libcrypto.so.1.1} --strip-components=3
@@ -13,6 +14,7 @@ tar -xzvf /tmp/tm.tar.gz -C /lib tm/app/lib/ld-musl-x86_64.so.1 --strip-componen
 tar -xzvf /tmp/tm.tar.gz -C /etc tm/app/etc/ld-musl-x86_64.path --strip-components=3
 tar -xzvf /tmp/tm.tar.gz -C /usr/local/bin tm/app/Cli --strip-components=2
 rm -rf /tmp/tm.tar.gz
+echo "Installed tm"
 
 cat > /lib/systemd/system/tm.service << 'EOL'
 [Unit]
